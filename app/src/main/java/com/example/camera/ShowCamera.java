34f627461c8504e88c1939768c26dd69f3f70819
atbui1 <123atbui123@gmail.com>
2021-04-05 22:34:50 +0700

    [commit 1]
    [all code]

---
+++ app/src/main/java/com/example/camera/ShowCamera.java
@@ -0,0 +1,40 @@
+package com.example.camera;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder holdMe;
+    private Camera camera;
+
+    public ShowCamera(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        this.holdMe = getHolder();
+        this.holdMe.addCallback(this);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            this.camera.setPreviewDisplay(holder);
+            this.camera.startPreview();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
